335dbfe289566bf4ba20fef5ac36e521567e3f32
loverbeeke <loverbeeke@deloitte.nl>
2021-12-09 15:38:19 +0100

    Login system

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print('test')
+UsernameList = ["jacob"]
+PasswordList = ["pass1"]
+
+print("Welcome to the Investment Game")
+userType = input("Do you have an account for this Investment Game? please enter y/n:")
+
+# Full name
+# Available amount of money
+# Currencies
+# Limits
+# More user variables?
+
+if userType == 'n':
+    User = input("Please type your username: ")
+    UsernameList.append(User)
+    Pass = input("Please type your password: ")
+    PasswordList.append(Pass)
+    print("You have created your new account with the Investment Game, please login\n")
+    UserName = input("Please enter your username: ")
+    Password = input("Please enter your password: ")
+    if UserName in UsernameList:  # is User1 in the list?
+        if Password == PasswordList[UsernameList.index(UserName)]:
+            print("Log in Success")
+    else:
+        print("Incorrect username or Password")
+else:
+    print("Please login\n")
+    UserName = input("Please enter your username: ")
+    Password = input("Please enter your password: ")
+    if UserName in UsernameList:  # is User1 in the list?
+        if Password == PasswordList[UsernameList.index(UserName)]:
+            print("Log in Success")
+        else:
+            print("Incorrect username or Password")
+    else:
+        print("Incorrect username or Password")
