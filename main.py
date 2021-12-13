a2c64b7c5c9761f6daee8c5385c820de9da4a833
loverbeeke <loverbeeke@deloitte.nl>
2021-12-13 15:48:50 +0100

    Final version

--- main.py
+++ main.py
@@ -257,9 +257,6 @@ def calculate_portfolio_balance():
         balance += (portfolio[x] * price)
     return balance
 
-#def calculate_portfolio_balance_over_time():
-
-
 def show_chart():
     df, symbol = get_stock_data()
     df[['open', 'high', 'low', 'close']].plot()
@@ -268,45 +265,25 @@ def show_chart():
 def button_buy():
     global df
     global symbol
+    print("\n")
     df, symbol = get_stock_data()
     return buy_stocks()
 
 def button_sell():
     global df
     global symbol
+    print("\n")
     df, symbol = get_stock_data()
     return sell_stocks()
+
+def show_portfolio():
+    print("\n")
+    portfolio_info()
 
 def menu():
     global wallet
     global symbol
     global df
-    # Show portfolio, Display chart of a share, Buy, sell, Quit
-    # print("\n--------- MENU ---------")
-    # print("1: Show portfolio")
-    # print("2: Get share info")
-    # print("3: Buy shares")
-    # print("4: Sell shares")
-    # print("5: Quit")
-    # print("----------------------\n")
-    # choice = int(input("To which page do you want to go? Enter a number: "))
-    # if choice == 1:
-    #     portfolio_info()
-    #     return_to_menu()
-    # elif choice == 2:
-    #     show_chart()
-    #     return_to_menu()
-    # elif choice == 3:
-    #     df, symbol = get_stock_data()
-    #     buy_stocks()
-    #     return_to_menu()
-    # elif choice == 4:
-    #     df, symbol = get_stock_data()
-    #     sell_stocks()
-    #     return_to_menu()
-    # else:
-    #     quit()
-
     #Adding GUI menu:
     global b1
     global b2
@@ -315,10 +292,10 @@ def menu():
     global b5
     window = tk.Tk()
     window.title('Investment Game')
-    greeting = tk.Label(text="MENU\n Please choose one of the following options:",
+    greeting = tk.Label(text="MENU\nPlease choose one of the following options:",
                         anchor='n', foreground="yellow", background="purple", width=100, height=10, pady=100)
     greeting.pack()
-    b1 = tk.Button(window, bg='yellow', text="Show portfolio", command=portfolio_info, wraplength=70).place(x=100, y=200)
+    b1 = tk.Button(window, bg='yellow', text="Show portfolio", command=show_portfolio, wraplength=70).place(x=100, y=200)
     b2 = tk.Button(window, bg='yellow', text="Get share info", command=show_chart, wraplength=70).place(x=200, y=200)
     b3 = tk.Button(window, bg='yellow', text="Buy shares", command=button_buy, height=2).place(x=300, y=200)
     b4 = tk.Button(window, bg='yellow', text="Sell shares", command=button_sell, height=2).place(x=400, y=200)
